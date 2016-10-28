d5e0c390e9012f920a352f8ea855a161bee7eb8f
mon9x <h.mon9x@gmail.com>
2016-10-29 06:05:13 +0700

    Add files via upload

---
+++ Evade/Benchmarking/Benchmark.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu.Values;
+using SharpDX;
+
+namespace Evade.Benchmarking
+{
+    public static class Benchmark
+    {
+        private static Vector2 startPoint;
+        private static Vector2 endPoint;
+
+        public static void Initialize()
+        {
+            Game.OnWndProc += Game_OnWndProc;
+        }
+
+        static void SpawnLineSkillShot(Vector2 start, Vector2 end)
+        {
+            SkillshotDetector.TriggerOnDetectSkillshot(
+                DetectionType.ProcessSpell, SpellDatabase.GetByName("TestLineSkillShot"), Utils.TickCount,
+                start, end, ObjectManager.Player);
+
+            Core.DelayAction(() => SpawnLineSkillShot(start, end), 5000);
+        }
+
+        static void SpawnCircleSkillShot(Vector2 start, Vector2 end)
+        {
+            SkillshotDetector.TriggerOnDetectSkillshot(
+                DetectionType.ProcessSpell, SpellDatabase.GetByName("TestCircleSkillShot"), Utils.TickCount,
+                start, end, ObjectManager.Player);
+
+            Core.DelayAction(() => SpawnCircleSkillShot(start, end), 5000);
+        }
+
+        private static void Game_OnWndProc(WndEventArgs args)
+        {
+            if (args.Msg == (uint)WindowMessages.LeftButtonDown)
+            {
+                startPoint = Game.CursorPos.To2D();
+            }
+
+            if (args.Msg == (uint)WindowMessages.LeftButtonUp)
+            {
+                endPoint = Game.CursorPos.To2D();
+            }
+
+            if (args.Msg == (uint)WindowMessages.KeyUp && args.WParam == 'L') //line missile skillshot
+            {
+                SpawnLineSkillShot(startPoint, endPoint);
+            }
+
+            if (args.Msg == (uint)WindowMessages.KeyDown && args.WParam == 'I') //circular skillshoot
+            {
+                SpawnCircleSkillShot(startPoint, endPoint);
+            }
+        }
+    }
+}
