9bcc8540c0fc4bed84a2a98db80f308e074011a2
mon9x <h.mon9x@gmail.com>
2016-12-03 02:05:06 +0700

    Update SpellDatabase.cs

--- Evade/SpellDatabase.cs
+++ Evade/SpellDatabase.cs
@@ -454,7 +454,7 @@ namespace Evade
                     Delay = 250,
                     Range = 1050,
                     Radius = 70,
-                    MissileSpeed = 1750,
+                    MissileSpeed = 1800,
                     FixedRange = true,
                     AddHitbox = true,
                     DangerValue = 4,
