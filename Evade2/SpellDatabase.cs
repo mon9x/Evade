90456175c85269be4e2a39195084c301f30e2911
mon9x <h.mon9x@gmail.com>
2017-02-03 16:15:50 +0700

    Update SpellDatabase.cs

--- Evade2/SpellDatabase.cs
+++ Evade2/SpellDatabase.cs
@@ -4569,6 +4569,30 @@ namespace Evade
                 });
 
             #endregion Zyra
+                
+            #region Warwick
+
+            Spells.Add(
+                new SpellData
+                {
+                    ChampionName = "Warwick",
+                    SpellName = "WarwickR",
+                    Slot = SpellSlot.Q,
+                    Type = SkillShotType.SkillshotMissileLine,
+                    Delay = 250,
+                    Range = 1300,
+                    Radius = 90,
+                    MissileSpeed = 2175,
+                    FixedRange = true,
+                    AddHitbox = true,
+                    DangerValue = 5,
+                    IsDangerous = true,
+                    MissileSpellName = "WarwickRMissile",
+                    EarlyEvade = new[] {EarlyObjects.Allies, EarlyObjects.AllyObjects},
+                    CollisionObjects = new[] {CollisionObjectTypes.YasuoWall, CollisionObjectTypes.Champions}
+                });
+
+            #endregion Warwick
 
             //Console.WriteLine("Added " + Spells.Count + " spells.");
         }
